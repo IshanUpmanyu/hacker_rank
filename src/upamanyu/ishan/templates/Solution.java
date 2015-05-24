9bc37d9f695a5fa68ff96e4a412e5229d4fc6832
ISHAN UPAMANYU <ishan.upmanyu@gmail.com>
2015-05-24 12:37:58 +0530

    Added the solution template

---
+++ src/upamanyu/ishan/templates/Solution.java
@@ -0,0 +1,14 @@
+package upamanyu.ishan.templates;
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.println();
+	}
+
+}
