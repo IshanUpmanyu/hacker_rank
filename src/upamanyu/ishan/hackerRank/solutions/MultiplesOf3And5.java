932d8d4c6641169502c0144258488cdb915fceda
ISHAN UPAMANYU <ishan.upmanyu@gmail.com>
2015-05-24 16:27:50 +0530

    Multiples of 3 and 5 changed to recursive version. Still TLE

--- src/upamanyu/ishan/hackerRank/solutions/MultiplesOf3And5.java
+++ src/upamanyu/ishan/hackerRank/solutions/MultiplesOf3And5.java
@@ -2,28 +2,36 @@ package upamanyu.ishan.hackerRank.solutions;
 
 import java.util.Scanner;
 
-public class MultiplesOf3And5{
-	
-	public static void main(String args[]){
+public class MultiplesOf3And5 {
+
+	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
-		
+
 		int sum = 0;
 		int n;
-		
+
 		for (int i = 0; i < t; i++) {
 			n = in.nextInt();
-			sum = sumOfMultiples(n);
+			sum = sumOfMultiples(1,n);
 			System.out.println(sum);
 		}
+		in.close();
 	}
 
-	private static int sumOfMultiples(int n) {
+	private static int sumOfMultiples(int a, int b) {
 		int sum = 0;
-		for(int i = 1; i<n; i++){
-			if((i%3==0)||(i%5 ==0)){
-				sum +=i;
+
+		if (a - b <= 5) {
+			for (int i = a; i < b; i++) {
+				if ((i % 3 == 0) || (i % 5 == 0)) {
+					sum += i;
+				}
 			}
+		} else {
+			int mid = (a+b)/2;
+			sum = sumOfMultiples(a, mid) 
+					+ sumOfMultiples(mid+1, b);
 		}
 		return sum;
 	}
