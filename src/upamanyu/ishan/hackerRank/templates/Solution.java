075734f7cdd534b03bea87dc6df7f410322fc9f5
ISHAN UPAMANYU <ishan.upmanyu@gmail.com>
2015-05-24 12:54:18 +0530

    Corrected formating of Solution Template

--- src/upamanyu/ishan/hackerRank/templates/Solution.java
+++ src/upamanyu/ishan/hackerRank/templates/Solution.java
@@ -3,23 +3,21 @@ package upamanyu.ishan.hackerRank.templates;
 import java.util.Scanner;
 
 public class Solution {
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
-		
+
 		int sum = 0;
-        int a,b;
-        t = in.nextInt();
-        for (int i=0;i<t;i++) {
-            a = in.nextInt();
-            b = in.nextInt();
-          //  sum = solveMeSecond(a, b);
-          //  System.out.println(sum);
-        }
+		int a, b;
+		t = in.nextInt();
+		for (int i = 0; i < t; i++) {
+			a = in.nextInt();
+			b = in.nextInt();
+			// sum = solveMeSecond(a, b);
+			// System.out.println(sum);
+		}
 		in.close();
 	}
 
 }
-
-
